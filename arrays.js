a02906aa9cb3d79d3a655ff46a42a77dab8f99cc
Heather Miller <heather.miller114@gmail.com>
2018-07-20 18:11:00 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -2,7 +2,7 @@ var chocolateBars = [ "snickers", "hundred grand", "kitkat", "skittles" ];
 console.log(chocolateBars)
   
 function addElementToBeginningOfArray(array, element) {
-  [ "${element}", ...array ]
+  [element, ...array ]
   return array
 }
 
