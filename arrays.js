b7f12fef4b325e6993d64b8b34cf5c533ca09d76
Heather Miller <heather.miller114@gmail.com>
2018-07-20 17:44:22 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -6,7 +6,7 @@ var chocolateBars = [
   ]
   
 function addElementToBeginningOfArray(array, element) {
-  ["element", ...array]
+  array.unshift("element")
   return array
 }
 function destructivelyAddElementToBeginningOfArray(array, element) {
