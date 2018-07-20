491e2bcf1a0420887143f73e5af7f18ef02925aa
Heather Miller <heather.miller114@gmail.com>
2018-07-20 18:40:24 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,19 +1,14 @@
 var chocolateBars = [ "snickers", "hundred grand", "kitkat", "skittles" ];
 console.log(chocolateBars)
 
-  
 function addElementToBeginningOfArray(array, element) {
   var array2 = [element, ...array]
   return array
 }
-
-
 function destructivelyAddElementToBeginningOfArray(array, element) {
   array.unshift(element)
   return array
 }
-
-
 function addElementToEndOfArray(array, element) {
   [...array, element]
   return array
