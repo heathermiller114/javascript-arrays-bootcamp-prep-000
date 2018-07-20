7b4509371744266a49dccaaa6f803a4a7ac693c6
Heather Miller <heather.miller114@gmail.com>
2018-07-20 18:07:43 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -6,5 +6,6 @@ function addElementToBeginningOfArray(array, element) {
   return array
 }
 function destructivelyAddElementToBeginningOfArray(array, element) {
-  [array = "element", ...array]
+  array.unshift(element)
+  return array
 }
